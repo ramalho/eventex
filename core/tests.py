47f83fe2708cc91a50f9b67398be3f60b6ea3199
Henrique Bastos <henrique@bastos.net>
2010-10-01 20:12:38 -0300

    Cria teste de url para o index

--- core/tests.py
+++ core/tests.py
@@ -1,23 +1,7 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
-
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
-
+class IndexUrlTest(TestCase):
+    def test_success_when_get_index_page(self):
+        response = self.client.get('/')
+        self.assertEquals(200, response.status_code)
+        self.assertTemplateUsed(response, 'index.html')
