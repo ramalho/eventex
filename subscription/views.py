89f97c1707e79e78a8f8755a377dec2ee1d39826
Henrique Bastos <henrique@bastos.net>
2010-10-12 20:47:24 -0300

    Adiciona cssr ao form

--- subscription/views.py
+++ subscription/views.py
@@ -1,10 +1,11 @@
 # coding: utf-8
 from django.http import HttpResponse
+from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 from subscription.forms import SubscriptionForm
 
 
 def new(request):
-    context = {'form': SubscriptionForm()}
+    context = RequestContext(request, {'form': SubscriptionForm()})
     return render_to_response('subscription/new.html', context)
