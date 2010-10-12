1d216bb4cbd95a6930f9846f7155bb0c36241d1f
Henrique Bastos <henrique@bastos.net>
2010-10-12 20:43:59 -0300

    Adiciona email, phone e created_at ao model Subscription

--- subscription/models.py
+++ subscription/models.py
@@ -3,4 +3,8 @@ from django.db import models
 
 class Subscription(models.Model):
     name = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=11)
+    cpf = models.CharField(max_length=11, unique=True)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+
