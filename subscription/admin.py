669ab1657fe44f7686de2befa4b7dbf98fb610c9
Flavio Amieiro <amieiro.flavio@gmail.com>
2010-10-15 18:00:40 -0300

    Adiciona admin de Subscription com List_display

---
+++ subscription/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from subscription.models import Subscription
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'cpf', 'email', 'phone', 'created_at')
+
+admin.site.register(Subscription, SubscriptionAdmin)
