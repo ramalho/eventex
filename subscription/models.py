9438dcd881a75105091a4debef2caafd3e1086d7
Flavio Amieiro <amieiro.flavio@gmail.com>
2010-10-15 18:22:17 -0300

    Adiciona um if ao template de sucesso

--- subscription/models.py
+++ subscription/models.py
@@ -30,7 +30,7 @@ class Subscription(models.Model):
     name = models.CharField('Nome', max_length=100)
     cpf = models.CharField('CPF', max_length=11, unique=True)
     email = models.EmailField('E-mail', unique=True)
-    phone = models.CharField('Telefone', max_length=20)
+    phone = models.CharField('Telefone', max_length=20, blank=True)
     created_at = models.DateTimeField('Criado em', auto_now_add=True)
 
     def __unicode__(self):
