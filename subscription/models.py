82978469bb79adbe25e9426ab67bc0ab0a7fbf2b
Flavio Amieiro <amieiro.flavio@gmail.com>
2010-10-15 18:00:10 -0300

    Adiciona verbose_name aos campos do model

--- subscription/models.py
+++ subscription/models.py
@@ -27,11 +27,11 @@ class Subscription(models.Model):
     IntegrityError: column email is not unique
 
     """
-    name = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=11, unique=True)
-    email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=20)
-    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField('Nome', max_length=100)
+    cpf = models.CharField('CPF', max_length=11, unique=True)
+    email = models.EmailField('E-mail', unique=True)
+    phone = models.CharField('Telefone', max_length=20)
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
 
     def __unicode__(self):
         return u'%s - %s - %s' % (self.pk, self.name, self.email)
