c54a7701240185f2bc1712439993cf4f790ee937
Flavio Amieiro <amieiro.flavio@gmail.com>
2010-10-15 17:04:31 -0300

    Usa template para construir a mensagem do e-mail

--- subscription/views.py
+++ subscription/views.py
@@ -2,7 +2,7 @@
 from django.core.urlresolvers import reverse
 from django.core.mail import send_mail
 from django.http import HttpResponseRedirect
-from django.template import RequestContext
+from django.template import RequestContext, Context, loader
 from django.shortcuts import render_to_response
 
 from subscription.forms import SubscriptionForm
@@ -20,13 +20,7 @@ def subscribe(request):
         form = SubscriptionForm(request.POST)
         if form.is_valid():
             subscription = form.save()
-            send_mail(
-                subject = u'Inscrição no EventeX',
-                message = u'Sua inscrição foi efetuada com sucesso',
-                from_email = 'contato@eventex.com',
-                recipient_list = [ subscription.email ],
-            )
-
+            send_subscription_email(subscription)
             return HttpResponseRedirect(reverse('subscription:success'))
     else:
         form = SubscriptionForm()
@@ -34,3 +28,15 @@ def subscribe(request):
     context = RequestContext(request, {'form': form})
     return render_to_response('subscription/new.html', context)
 
+def send_subscription_email(subscription):
+    # render_to_string
+    message_template = loader.get_template('subscription/subscription_email.txt')
+    context = Context({'subscription': subscription})
+    message = message_template.render(context)
+
+    send_mail(
+        subject = u'Inscrição no EventeX',
+        message = message,
+        from_email = 'contato@eventex.com',
+        recipient_list = [ subscription.email ],
+    )
