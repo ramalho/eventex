6a6b82013f930546669eb41cf010125ab484bcf8
Flavio Amieiro <amieiro.flavio@gmail.com>
2010-10-15 16:38:19 -0300

    Ajusta identação dos testes

--- subscription/tests/test_urls.py
+++ subscription/tests/test_urls.py
@@ -12,14 +12,11 @@ class SubscriptionUrlTest(TestCase):
         self.assertTrue(isinstance(response.context['form'], SubscriptionForm))
 
     def test_redirects_to_success_after_valid_post(self):
-        response = self.client.post(
-            reverse('subscription:subscribe'),
-            {
-                'name': 'Guido Van Rossum',
-                'cpf': '11111111111',
-                'email': 'bdfl@python.org',
-                'phone': '+1 754 3020 2000'
-            }
-        )
+        response = self.client.post(reverse('subscription:subscribe'), {
+            'name': 'Guido Van Rossum',
+            'cpf': '11111111111',
+            'email': 'bdfl@python.org',
+            'phone': '+1 754 3020 2000'
+        })
         self.assertRedirects(response, reverse('subscription:success'))
 
--- subscription/tests/test_views.py
+++ subscription/tests/test_views.py
@@ -14,16 +14,12 @@ class SubscriptionViewTest(TestCase):
         self.assertTrue(response.context['form'].errors)
 
     def test_if_subscription_is_saved_after_successful_post(self):
-
         self.assertFalse(Subscription.objects.exists())
-        response = self.client.post(
-            reverse('subscription:subscribe'),
-            {
-                'name': 'Guido Van Rossum',
-                'cpf': '11111111111',
-                'email': 'bdfl@python.org',
-                'phone': '+1 754 3020 2000'
-            }
-        )
+        response = self.client.post(reverse('subscription:subscribe'), {
+            'name': 'Guido Van Rossum',
+            'cpf': '11111111111',
+            'email': 'bdfl@python.org',
+            'phone': '+1 754 3020 2000'
+        })
         self.assertRedirects(response, reverse('subscription:success'))
         self.assertTrue(Subscription.objects.exists())
