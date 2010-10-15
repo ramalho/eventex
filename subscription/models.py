2d5af51be0f0cf0d6190c8bf09c333185c5db83c
Flavio Amieiro <amieiro.flavio@gmail.com>
2010-10-15 17:26:30 -0300

    Conecta o handler que envia o e-mail ao sinal post_save do model

--- subscription/models.py
+++ subscription/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from subscription.receivers import send_subscription_email
 
 class Subscription(models.Model):
     name = models.CharField(max_length=100)
@@ -8,3 +8,4 @@ class Subscription(models.Model):
     phone = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
 
+models.signals.post_save.connect(send_subscription_email, sender=Subscription)
