97a3b5c6c19623a7d3266662808004ad22c8f45d
Henrique Bastos <henrique@bastos.net>
2010-10-16 01:10:59 -0300

    MEDIA_URL no context manualmente

--- core/views.py
+++ core/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render_to_response
 
 
 def homepage(request):
-    return render_to_response('index.html')
+    from django.conf import settings
+    context = {'MEDIA_URL': settings.MEDIA_URL}
+
+    return render_to_response('index.html', context)
