040d03fde8580108619d3d46b94733a92a9a9fd6
Henrique Bastos <henrique@bastos.net>
2010-10-16 01:06:56 -0300

    Render_to_response

--- core/views.py
+++ core/views.py
@@ -1,11 +1,5 @@
-
-from django.http import HttpResponse
-from django.template import loader, Context
+from django.shortcuts import render_to_response
 
 
 def homepage(request):
-    t = loader.get_template('index.html')
-    c = Context()
-    content = t.render(c)
-    return HttpResponse(content)
-
+    return render_to_response('index.html')
