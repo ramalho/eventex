786862b9cf82b36f5a5e6b890c382e41e5543815
Henrique Bastos <henrique@bastos.net>
2010-10-16 01:05:58 -0300

    Renderizando template na mão

--- core/views.py
+++ core/views.py
@@ -1,5 +1,11 @@
+
 from django.http import HttpResponse
+from django.template import loader, Context
 
 
 def homepage(request):
-    return HttpResponse('Bem-vindo ao EventeX!')
+    t = loader.get_template('index.html')
+    c = Context()
+    content = t.render(c)
+    return HttpResponse(content)
+
