c1dacf4b497dc1d7eb578c2452c79850ac752e19
Flavio Amieiro <amieiro.flavio@gmail.com>
2010-10-26 20:51:04 -0200

    Adiciona campo boleano (pago) no model de inscrição (com a migração correspondente)

--- subscription/models.py
+++ subscription/models.py
@@ -32,6 +32,7 @@ class Subscription(models.Model):
     email = models.EmailField('E-mail', unique=True)
     phone = models.CharField('Telefone', max_length=20, blank=True)
     created_at = models.DateTimeField('Criado em', auto_now_add=True)
+    paid = models.BooleanField('Pago')
 
     def __unicode__(self):
         return u'%s - %s - %s' % (self.pk, self.name, self.email)
