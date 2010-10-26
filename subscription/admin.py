a62441c823dce07e44720584f3a07347cf091f96
Flavio Amieiro <amieiro.flavio@gmail.com>
2010-10-26 21:02:27 -0200

    Adiciona vizualização do campo boleano 'pago' na lista do admin

--- subscription/admin.py
+++ subscription/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from subscription.models import Subscription
 
 class SubscriptionAdmin(admin.ModelAdmin):
-    list_display = ('name', 'cpf', 'email', 'phone', 'created_at')
+    list_display = ('name', 'cpf', 'email', 'phone', 'created_at', 'paid')
 
     date_hierarchy = 'created_at'
 
