5689d79991ff461fd12e0f22cc7d5b330568ea9b
Flavio Amieiro <amieiro.flavio@gmail.com>
2010-10-27 12:56:22 -0200

    Adiciona validação para que o cpf tenha exatamente 11 dígitos

--- subscription/forms.py
+++ subscription/forms.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 from django import forms
 
 from subscription.models import Subscription
@@ -7,3 +8,9 @@ class SubscriptionForm(forms.ModelForm):
     class Meta:
         model = Subscription
         exclude = ('created_at', 'paid')
+
+    def clean_cpf(self):
+        data = self.cleaned_data['cpf']
+        if len(data) != 11:
+            raise forms.ValidationError('O CPF deve ter 11 dígitos')
+        return data
--- subscription/tests/test_forms.py
+++ subscription/tests/test_forms.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 from django.test import TestCase
 from subscription.forms import SubscriptionForm
 
@@ -6,3 +7,13 @@ class SubscriptionFormTest(TestCase):
     def test_if_form_has_no_paid_field(self):
         form =  SubscriptionForm()
         self.assertNotIn('paid', form.fields)
+
+    def test_returns_error_with_less_than_11_digits_in_cpf(self):
+        form = SubscriptionForm({
+            'name': 'User',
+            'cpf': '123456789',
+            'email': 'user@gmail.com',
+            'phone': '555-78376',
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('cpf', form.errors)
