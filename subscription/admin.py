e5f10f8dc1e1faed36f6c3e56661f67fe91915b2
Flavio Amieiro <amieiro.flavio@gmail.com>
2010-10-27 14:51:47 -0200

    Adiciona url nova no admin, com uma view placeholder

--- subscription/admin.py
+++ subscription/admin.py
@@ -1,5 +1,7 @@
 #-*- coding: utf-8 -*-
+from django.conf.urls.defaults import patterns, url
 from django.contrib import admin
+from django.http import HttpResponse
 
 from subscription.models import Subscription
 
@@ -21,7 +23,20 @@ class SubscriptionAdmin(admin.ModelAdmin):
         else:
             msg = '%d inscrições foram marcadas como pagas' % subscriptions_updated
         self.message_user(request, msg)
-
     mark_as_paid.short_description = u"Marcar inscrições como pagas"
+
+    def get_urls(self):
+        original_urls = super(SubscriptionAdmin, self).get_urls()
+        extra_url = patterns('',
+            # Envolvemos nossa view em 'admin_view' por que ela faz o
+            # controle de permissões e cache automaticamente para nós.
+            url(r'exportar-inscricoes/$', self.admin_site.admin_view(self.export_subscriptions), name='export_subscriptions')
+        )
+        # A ordem é importante. As URLs originais do admin são muito permissivas
+        # e acabam sendo encontradas antes da nossa se elas estiverem na frente.
+        return extra_url + original_urls
+
+    def export_subscriptions(self, request):
+        return HttpResponse('Placeholder')
 
 admin.site.register(Subscription, SubscriptionAdmin)
