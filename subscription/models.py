5b4972a982925c0ef9a4941db9fa343863ff24ce
Henrique Bastos <henrique@bastos.net>
2010-10-28 10:53:51 -0200

    Extrai o cpf_validator para um módulo próprio.

--- subscription/models.py
+++ subscription/models.py
@@ -1,14 +1,7 @@
 #-*- coding: utf-8 -*-
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django.core.exceptions import ValidationError
-
-
-def cpf_validator(value):
-    if not value.isdigit():
-        raise ValidationError(_(u'O CPF deve conter apenas números'))
-    if len(value) != 11:
-        raise ValidationError(_(u'O CPF deve ter 11 dígitos'))
+from subscription.validators import cpf_validator
 
 
 class Subscription(models.Model):
