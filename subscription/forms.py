e7e06a005df6da6e309565007f7f6b96e66d1653
Henrique Bastos <henrique@bastos.net>
2010-10-29 07:40:24 -0200

    Ajusta parâmetros do field CPF

--- subscription/forms.py
+++ subscription/forms.py
@@ -5,9 +5,10 @@ from django.utils.translation import ugettext_lazy as _
 from subscription import validators
 
 
+#5 - CpfValidator
 class SubscriptionForm(forms.Form):
     name = forms.CharField(label=_('Nome'), max_length=100)
-    cpf = forms.CharField(label=_('CPF'), max_length=11, min_length=11, validators=[validators.CpfValidator])
+    cpf = forms.CharField(label=_('CPF'), max_length=11, validators=[validators.CpfValidator])
     email = forms.EmailField(label=_('E-mail'))
     phone = forms.CharField(label=_('Telefone'), required=False, max_length=20)
 
