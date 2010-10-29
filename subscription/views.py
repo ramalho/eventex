d9a43ded89f4eff26d9839ea62e76b8b2cd7903d
Henrique Bastos <henrique@bastos.net>
2010-10-29 07:40:53 -0200

    Adiciona initial no form Subscription.

--- subscription/views.py
+++ subscription/views.py
@@ -9,7 +9,12 @@ from subscription.utils import send_subscription_email
 
 
 def new(request):
-    form = SubscriptionForm()
+    form = SubscriptionForm(initial={
+        'name': 'Entre o seu nome',
+        'cpf': 'Digite o seu CPF sem pontos',
+        'email': 'Informe o seu email',
+        'phone': 'Qual seu telefone de contato?',
+    })
     context = RequestContext(request, {'form': form})
     return render_to_response('subscription/new.html', context)
 
