3571f3b569e68006c2d879a552bc8b6ae7e233db
Henrique Bastos <henrique@bastos.net>
2010-10-29 07:39:58 -0200

    Corrige ordem dos parâmetros do phone

--- subscription/forms.py
+++ subscription/forms.py
@@ -9,7 +9,7 @@ class SubscriptionForm(forms.Form):
     name = forms.CharField(label=_('Nome'), max_length=100)
     cpf = forms.CharField(label=_('CPF'), max_length=11, min_length=11, validators=[validators.CpfValidator])
     email = forms.EmailField(label=_('E-mail'))
-    phone = forms.CharField(required=False, label=_('Telefone'), max_length=20)
+    phone = forms.CharField(label=_('Telefone'), required=False, max_length=20)
 
 """
 #4 - Mais parâmetros que refletem validação.
@@ -17,14 +17,14 @@ class SubscriptionForm(forms.Form):
     name = forms.CharField(label=_('Nome'), max_length=100)
     cpf = forms.CharField(label=_('CPF'), max_length=11, min_length=11)
     email = forms.EmailField(label=_('E-mail'))
-    phone = forms.CharField(required=False, label=_('Telefone'), max_length=20)
+    phone = forms.CharField(label=_('Telefone'), required=False, max_length=20)
 
 #3 - Ajustando label e alinhando required.
 class SubscriptionForm(forms.Form):
     name = forms.CharField(label=_('Nome'))
     cpf = forms.CharField(label=_('CPF'))
     email = forms.EmailField(label=_('E-mail'))
-    phone = forms.CharField(required=False, label=_('Telefone'))
+    phone = forms.CharField(label=_('Telefone'), required=False)
 
 #2 - Form básico feito na mão
 class SubscriptionForm(forms.Form):
