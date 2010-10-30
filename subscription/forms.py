d7285b15e1e37da98c140ce04a944c8aacaf5d47
Henrique Bastos <henrique@bastos.net>
2010-10-30 08:40:10 -0200

    Volta para o modelform padrão.

--- subscription/forms.py
+++ subscription/forms.py
@@ -6,41 +6,8 @@ from subscription import validators
 from django.core.validators import EMPTY_VALUES
 
 
-class PhoneWidget(forms.MultiWidget):
-    def __init__(self, attrs=None):
-        widgets = (
-            forms.TextInput(attrs=attrs),
-            forms.TextInput(attrs=attrs))
-        super(PhoneWidget, self).__init__(widgets, attrs)
-
-    def decompress(self, value):
-        if value:
-            return value.split('-')
-        return [None, None]
-
-class PhoneField(forms.MultiValueField):
-    widget = PhoneWidget
-
-    def __init__(self, *args, **kwargs):
-        fields = (
-            forms.IntegerField(),
-            forms.IntegerField())
-        super(PhoneField, self).__init__(fields, *args, **kwargs)
-
-    def compress(self, data_list):
-        if data_list:
-            if data_list[0] in EMPTY_VALUES:
-                raise forms.ValidationError(u'DDD inválido.')
-            if data_list[1] in EMPTY_VALUES:
-                raise forms.ValidationError(u'Número inválido.')
-            return '%s-%s' % tuple(data_list)
-        return None
-
-
 #6 - ModelForm completo
 class SubscriptionForm(forms.ModelForm):
-    phone = PhoneField()
-
     class Meta:
         model = Subscription
         exclude = ('created_at', 'paid')
