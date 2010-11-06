4b4641ed0d968bbc37f36f92e951ed597db0996f
Henrique Bastos <henrique@bastos.net>
2010-11-06 08:37:49 -0200

    Desliga o south

--- settings.py
+++ settings.py
@@ -97,7 +97,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'south',
+    #'south',
     'core',
     'subscription',
     'embed',
