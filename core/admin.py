a80c2f37fd78722b4dde3c2e1a353ade3dc736ef
Henrique Bastos <henrique@bastos.net>
2010-11-06 01:25:40 -0200

    Registra Media como Inline no Admin

--- core/admin.py
+++ core/admin.py
@@ -9,7 +9,16 @@ class SpeakerAdmin(admin.ModelAdmin):
     inlines = [ContactInline, ]
     prepopulated_fields = {'slug': ('name', )}
 
+
+class MediaInline(admin.TabularInline):
+    model = Media
+    extra = 1
+
+class TalkAdmin(admin.ModelAdmin):
+    inlines = [MediaInline, ]
+
+
 admin.site.register(Speaker, SpeakerAdmin)
-admin.site.register(Talk)
+admin.site.register(Talk, TalkAdmin)
 admin.site.register(Course)
 admin.site.register(Media)
