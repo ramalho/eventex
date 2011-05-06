484ea5c38b61369d3c3125e8870d8add418afc5e
Henrique Bastos <henrique@bastos.net>
2011-05-05 21:21:54 -0300

    Corrige o context.

--- core/views.py
+++ core/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render_to_response
 
 def homepage(request):
     from django.conf import settings
-    context = {'MEDIA_URL': settings.MEDIA_URL}
+    context = {'STATIC_URL': settings.STATIC_URL}
 
     return render_to_response('index.html', context)
