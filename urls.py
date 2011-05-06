f887fba0acd9403a88991730db28d2256d0dc4d3
Henrique Bastos <henrique@bastos.net>
2011-05-05 21:20:47 -0300

    Ajusta urls.py

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls.defaults import *
-from core.views import *
+from django.conf.urls.defaults import patterns
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from core.views import homepage
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -17,9 +18,18 @@ urlpatterns = patterns('',
     (r'^$', homepage),
 )
 
-from django.conf import settings
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^media/(?P<path>.*)$', 'django.views.static.serve',
-        { 'document_root': settings.MEDIA_ROOT }),
-    )
+urlpatterns += staticfiles_urlpatterns()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^project/', include('project.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # (r'^admin/', include(admin.site.urls)),
+    (r'^$', homepage),
+)
+
+urlpatterns += staticfiles_urlpatterns()
