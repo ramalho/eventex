31543a5e96174d0cd6e3dec9af2fd9e276c3a9a4
Henrique Bastos <henrique@bastos.net>
2011-05-22 07:13:30 -0300

    Ajusta urs.py

--- urls.py
+++ urls.py
@@ -1,26 +1,14 @@
-from django.conf.urls.defaults import *
-
-
-# Uncomment the next two lines to enable the admin:
+from django.conf.urls.defaults import patterns, include, urls
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^project/', include('project.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
     url(r'^$', 'core.views.homepage', name='homepage'),
     (r'^inscricao/', include('subscription.urls', namespace='subscription')),
 )
 
-from django.conf import settings
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^media/(?P<path>.*)$', 'django.views.static.serve',
-        { 'document_root': settings.MEDIA_ROOT }),
-    )
+urlpatterns += staticfiles_urlpatterns()
