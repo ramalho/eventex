165de0847b74ad84f0c3de713e13af2522d0bf6e
Henrique Bastos <henrique@bastos.net>
2011-05-22 07:15:38 -0300

    Ajusta template para STATIC_URL

--- core/views.py
+++ core/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render_to_response
 
 def homepage(request):
     from django.conf import settings
-    context = {'MEDIA_URL': settings.MEDIA_URL}
+    context = {
+        'STATIC_URL': settings.STATIC_URL,
+        'MEDIA_URL': settings.MEDIA_URL,
+    }
 
     return render_to_response('index.html', context)
