d9be7cd2848508d57d61e16ee42ab8dcda287ba5
Henrique Bastos <henrique@bastos.net>
2011-05-29 01:22:47 -0300

    Refatora talks para class-based generic views com TemplateView.

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls.defaults import patterns, url
 from views import HomepageView
+from views import TalkListView
 
 urlpatterns = patterns('core.views',
-    url(r'^palestras/$', 'talks', name='talks'),
     url(r'^palestra/(\d+)/$', 'talk_details', name='talk_details'),
     url(r'^palestrante/([-\w]+)/$', 'speaker', name='speaker'),
     url(r'^$', HomepageView.as_view(), name='homepage'),
+    url(r'^palestras/$', TalkListView.as_view(), name='talks'),
 )
