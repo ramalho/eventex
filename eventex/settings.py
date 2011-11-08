9a87e6a95f4c7bdebdf256d424bd6062f52cc760
Flavio Amieiro <amieiro.flavio@gmail.com>
2011-11-08 14:46:28 -0200

    Altera configuração de SMTP para usar o gmail
    Conflicts:
    
    	eventex/settings.py

--- eventex/settings.py
+++ eventex/settings.py
@@ -22,7 +22,11 @@ DATABASES = {
 }
 
 DEFAULT_FROM_EMAIL = 'contato@eventex.com'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
